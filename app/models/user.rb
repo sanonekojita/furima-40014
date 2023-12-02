3c366a006b4d381ebdfb39d86c3786bd1ae0b364
sanonekojita <sano.nekojita@gmail.com>
2023-12-02 18:04:24 +0900

    validate修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,11 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 
   validates :last_name, presence: true
-  LAST_NAME_REGEX = /\A[一-龯ぁ-んァ-ヶー]+\z/
+  LAST_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
   validates_format_of :last_name, with: LAST_NAME_REGEX, message: 'is invalid. Input full-width characters'
 
   validates :first_name, presence: true
-  FIRST_NAME_REGEX = /\A[一-龯ぁ-んァ-ヶー]+\z/
+  FIRST_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
   validates_format_of :first_name, with: FIRST_NAME_REGEX, message: 'is invalid. Input full-width characters'
 
   validates :last_name_kana, presence: true
