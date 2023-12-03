c91bdd0642e3493b4d4d094826888fa74b0ae90e
sanonekojita <sano.nekojita@gmail.com>
2023-12-03 14:55:01 +0900

    ActiveHash・ActiveStorage 導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :item_price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  # has_one :purchase_record
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status, class_name: 'SalesStatus'
+  belongs_to :shipping_fee_status, class_name: 'ScheduledDelivery'
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery, class_name: 'ShippingFeeStatus'
+
+  #空の投稿を保存できないようにする
+  validates :item_name, :item_info, presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
