c5c0f2426d5e4b9486f32ed401309318460cd84f
sanonekojita <sano.nekojita@gmail.com>
2023-12-10 08:24:40 +0900

    ログアウト時⇒編集ページ⇒ログインページでログインしたときログアウト時のアクションが実行されるように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
 
   private
 
@@ -15,5 +20,13 @@ class ApplicationController < ActionController::Base
                                       keys: [:email, :password, :password_confirmation,
                                              :nickname, :last_name, :first_name, :last_name_kana,
                                              :first_name_kana, :birth_date])
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
   end
 end
