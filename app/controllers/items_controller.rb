69d063063a60c88a25c31293355725df9cc9bf61
sanonekojita <sano.nekojita@gmail.com>
2023-12-10 08:53:17 +0900

    Rubocop実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to action: :index
-    end
+    return if user_signed_in? && current_user.id == @item.user_id
+
+    redirect_to action: :index
   end
 
   def update
@@ -37,7 +37,6 @@ class ItemsController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
-
 
   private
 
