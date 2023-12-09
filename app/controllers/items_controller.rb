a5d41cf7625cf03454e49ef39e5ae3888053ffb0
sanonekojita <sano.nekojita@gmail.com>
2023-12-10 08:21:38 +0900

    編集関連ルーティング・コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item) unless @item.previous_changes.empty?
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
   private
 
   def item_params
