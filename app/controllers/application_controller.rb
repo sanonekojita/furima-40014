7266c1b081f69890bdf7552082307f2be592ff0b
sanonekojita <sano.nekojita@gmail.com>
2023-12-10 15:14:52 +0900

    遷移制限削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :store_user_location!, if: :storable_location?
 
   def after_sign_in_path_for(resource_or_scope)
     stored_location_for(resource_or_scope) || super
@@ -20,13 +19,5 @@ class ApplicationController < ActionController::Base
                                       keys: [:email, :password, :password_confirmation,
                                              :nickname, :last_name, :first_name, :last_name_kana,
                                              :first_name_kana, :birth_date])
-  end
-
-  def store_user_location!
-    store_location_for(:user, request.fullpath)
-  end
-
-  def storable_location?
-    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
   end
 end
