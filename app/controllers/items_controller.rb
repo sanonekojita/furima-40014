c1d06029f3e9b6a32ea809f9e898ae6971c2074f
sanonekojita <sano.nekojita@gmail.com>
2023-12-10 16:19:29 +0900

    削除機能ルーティング・コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+
+    return unless user_signed_in? && current_user.id == item.user_id
+
+    item.destroy
+    redirect_to root_path
+  end
 
   private
 
