551c6ac9d2766be7bd9ba15184f9c8003e635406
sanonekojita <sano.nekojita@gmail.com>
2023-12-15 14:27:49 +0900

    売り切れ時の表示

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,9 +1,15 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :move_to_sign_in
 
   def index
-    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @item = Item.find(params[:item_id])
     @purchase_record_shipping_address = PurchaseRecordShippingAddress.new
+
+    return unless user_signed_in? && current_user.id == @item.user_id || PurchaseRecord.exists?(item_id: @item.id)
+
+    redirect_to root_path
+    nil
   end
 
   def create
@@ -15,6 +21,7 @@ class PurchaseRecordsController < ApplicationController
       @purchase_record_shipping_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render  'purchase_records/index', status: :unprocessable_entity
     end
   end
@@ -22,11 +29,15 @@ class PurchaseRecordsController < ApplicationController
   private
 
   def purchase_record_params
-    params.require(:purchase_record_shipping_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id).merge(item_id: @item.id).merge(token: params[:token])
+    params.require(:purchase_record_shipping_address)
+          .permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+          .merge(user_id: current_user.id)
+          .merge(item_id: @item.id)
+          .merge(token: params[:token])
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.item_price,
       card: purchase_record_params[:token],
@@ -34,4 +45,9 @@ class PurchaseRecordsController < ApplicationController
     )
   end
 
+  def move_to_sign_in
+    return if user_signed_in?
+
+    redirect_to new_user_session_path, notice: 'You need to sign in or sign up before continuing.'
+  end
 end
