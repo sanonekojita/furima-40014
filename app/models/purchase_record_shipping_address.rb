a5e35cce86ad13d3190f92966c7f8ea87f7ce396
sanonekojita <sano.nekojita@gmail.com>
2023-12-16 19:06:23 +0900

    validates :prefecture_idをwith_options内に

--- app/models/purchase_record_shipping_address.rb
+++ app/models/purchase_record_shipping_address.rb
@@ -8,9 +8,8 @@ class PurchaseRecordShippingAddress
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
   end
 
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-
   with_options presence: true do
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city, :addresses
     validates :phone_number, length: { minimum: 10, message: 'is too short' },
                              format: { with: /\A[0-9]+\z/, message: 'is invalid. Input only number' }
