15c53a462e35a706b47619c0b99a1ab7ccb9052d
sanonekojita <sano.nekojita@gmail.com>
2023-12-16 19:03:33 +0900

    merge記述のまとめ

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -34,9 +34,7 @@ class PurchaseRecordsController < ApplicationController
   def purchase_record_params
     params.require(:purchase_record_shipping_address)
           .permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
-          .merge(user_id: current_user.id)
-          .merge(item_id: @item.id)
-          .merge(token: params[:token])
+          .merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def pay_item
