dbd3588a2aa443768853c710dbb5c4193b15ae69
sanonekojita <sano.nekojita@gmail.com>
2023-12-20 00:16:16 +0900

    修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,40 +1,43 @@
 let payjpInstance = null;
 
 const pay = () => {
-  const publicKey = gon.public_key;
 
-  if (!payjpInstance) {
-    payjpInstance = Payjp(publicKey);
-  }
-
-  const elements = payjpInstance.elements();
-  const numberElement = elements.create('cardNumber');
-  const expiryElement = elements.create('cardExpiry');
-  const cvcElement = elements.create('cardCvc');
-
-  numberElement.mount('#number-form');
-  expiryElement.mount('#expiry-form');
-  cvcElement.mount('#cvc-form');
-  
   const form = document.getElementById('charge-form');
 
-  form.addEventListener("submit", (e) => {
-    payjpInstance.createToken(numberElement).then(function (response) {
-      if (response.error) {
-      } else {
-        const token = response.id;
-        const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden">`;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
-      }
-      numberElement.clear();
-      expiryElement.clear();
-      cvcElement.clear();
-      document.getElementById("charge-form").submit();
-    });
+  if (form) {
 
-    e.preventDefault();
-  });
+    if (!payjpInstance) {
+      const publicKey = gon.public_key;
+      payjpInstance = Payjp(publicKey);
+    }
+
+    const elements = payjpInstance.elements();
+    const numberElement = elements.create('cardNumber');
+    const expiryElement = elements.create('cardExpiry');
+    const cvcElement = elements.create('cardCvc');
+
+    numberElement.mount('#number-form');
+    expiryElement.mount('#expiry-form');
+    cvcElement.mount('#cvc-form');
+    
+    form.addEventListener("submit", (e) => {
+      payjpInstance.createToken(numberElement).then(function (response) {
+        if (response.error) {
+        } else {
+          const token = response.id;
+          const renderDom = document.getElementById("charge-form");
+          const tokenObj = `<input value=${token} name='token' type="hidden">`;
+          renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        }
+        numberElement.clear();
+        expiryElement.clear();
+        cvcElement.clear();
+        document.getElementById("charge-form").submit();
+      });
+
+      e.preventDefault();
+    });
+  }
 };
 
 window.addEventListener("turbo:load", pay);
--- app/javascript/items_new.js
+++ app/javascript/items_new.js
@@ -1,19 +1,23 @@
-function post (){
-  const itemPrice  = document.getElementById("item-price");
-  itemPrice.addEventListener("keyup", () => {
-    const addTaxPrice  = document.getElementById("add-tax-price");
-    const profit  = document.getElementById("profit");
+function post() {
+  const itemPrice = document.getElementById("item-price");
 
-    const TaxPrice = parseInt(itemPrice.value / 10);
-    const Profit = itemPrice.value - TaxPrice
-    const formattedTaxPrice = TaxPrice.toLocaleString();
-    const formattedProfit = Profit.toLocaleString();
+  if (itemPrice) {
+    itemPrice.addEventListener("keyup", () => {
+      const addTaxPrice = document.getElementById("add-tax-price");
+      const profit = document.getElementById("profit");
 
+      if (addTaxPrice && profit) {
+        const TaxPrice = parseInt(itemPrice.value / 10);
+        const Profit = itemPrice.value - TaxPrice;
+        const formattedTaxPrice = TaxPrice.toLocaleString();
+        const formattedProfit = Profit.toLocaleString();
 
-    addTaxPrice.innerHTML = `${formattedTaxPrice}`;
-    profit.innerHTML = `${formattedProfit}`;
-  });
-};
+        addTaxPrice.innerHTML = `${formattedTaxPrice}`;
+        profit.innerHTML = `${formattedProfit}`;
+      }
+    });
+  }
+}
 
 window.addEventListener('turbo:load', post);
 window.addEventListener("turbo:render", post);
