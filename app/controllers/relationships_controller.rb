c1a4ecbd95c62b4e3fb3a93060375072380bf9dd
sanonekojita <sano.nekojita@gmail.com>
2024-01-07 16:51:24 +0900

    rubocop実施

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,9 +1,8 @@
 class RelationshipsController < ApplicationController
-
   def create
     follow = current_user.active_relationships.new(follower_id: params[:user_id])
     follow.save
-    if request.referer && request.referer.include?("items")
+    if request.referer&.include?('items')
       redirect_to item_path(params[:item_id])
     else
       redirect_to user_path(params[:user_id])
@@ -13,11 +12,10 @@ class RelationshipsController < ApplicationController
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    if request.referer && request.referer.include?("items")
+    if request.referer&.include?('items')
       redirect_to item_path(params[:item_id])
     else
       redirect_to user_path(params[:user_id])
     end
   end
-
 end
